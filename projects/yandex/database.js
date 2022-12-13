874b826a51381e6fb0667ec2ab47201fe0dc692b
annalebedeva <zharnova_bmstu@mail.ru>
2022-12-13 09:51:09 +0300

    Yandex task

---
+++ projects/yandex/database.js
@@ -0,0 +1,8 @@
+export const getReviews = () => {
+  const reviews = localStorage.reviews;
+  return JSON.parse(reviews || '[]');
+};
+
+export const addReview = (review) => {
+  localStorage.reviews = JSON.stringify([...getReviews(), review]);
+};
